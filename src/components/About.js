f45b931cdedd23022e76eeec64141e2ceba2f099
muhammad khizar <khizarislam98@gmail.com>
2019-08-31 21:05:09 +0500

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="contaianer">
+      <h4 className="center">About</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae incidunt placeat necessitatibus perspiciatis. Dolore, amet blanditiis saepe nulla at, veritatis hic quas rem perferendis nisi natus. Natus blanditiis reprehenderit molestias?</p>
+    </div>    
+  )
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="contaianer">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae incidunt placeat necessitatibus perspiciatis. Dolore, amet blanditiis saepe nulla at, veritatis hic quas rem perferendis nisi natus. Natus blanditiis reprehenderit molestias?</p>
+    </div>    
+  )
+}
+
+export default Contact;
